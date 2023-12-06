dbd5c9f6b230738df4e96cf4d4a45d8b5cb6edaf
vibhoraz400 <vibhoraz400@outlook.com>
2023-12-06 17:56:43 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using DBFIrstGenerate.VBScaffoldContext;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        using(EmpdbContext context=new EmpdbContext())
+        {
+            var data = context.Emps.ToList();
+            foreach(var item in data)
+            {
+                Console.WriteLine(item.Id+"   "+item.Name+"  "+item.Address);
+            }
+        }
+    }
+}
---
+++ VBEntityModels/Emp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DBFIrstGenerate.VBEntityModels;
+
+public partial class Emp
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Address { get; set; } = null!;
+}
+
